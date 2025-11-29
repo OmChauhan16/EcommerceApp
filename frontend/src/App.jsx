4eb5f1e674035d5a187aa0fa214d71be99cdb2a1
OmChauhan16 <omc5246@gmail.com>
2025-11-30 00:26:52 +0530

    Admin Sidebar and Homepage done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import OrderConfirmationPage from './pages/OrderConfirmationPage'
 import OrderDetailsPage from './pages/OrderDetailsPage'
 import MyOrdersPage from './pages/MyOrdersPage'
 import AdminLayout from './components/Admin/AdminLayout'
+import AdminHomePage from './pages/AdminHomePage'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -34,7 +35,9 @@ function App() {
             <Route path='order-confirmation' element={<OrderConfirmationPage />} />
             <Route path='order/:id' element={<OrderDetailsPage />} />
             <Route path='my-orders' element={<MyOrdersPage />} />
-            <Route path='/admin' element={<AdminLayout />} />
+            <Route path='/admin' element={<AdminLayout />} >
+             <Route index element={<AdminHomePage />} />
+            </Route>
           </Route>
           {/* Admin Layout */}
         </Routes>
