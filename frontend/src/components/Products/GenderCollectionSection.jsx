c526bf76f23cc06e9a6c4dde75e4c83a2764abf2
OmChauhan16 <omc5246@gmail.com>
2025-12-21 23:10:15 +0530

    redirection fixed

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -26,7 +26,7 @@ const GenderCollectionSection = () => {
                         <h2 className='text-2xl font-bold text-gray-900 mb-3'>
                             Men's Collection
                         </h2>
-                        <Link to="/collections/all?gender=Women"
+                        <Link to="/collections/all?gender=Men"
                             className="text-gray-900 underline">
                             Shop Now
                         </Link>
